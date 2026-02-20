a45c24546aa7e6ec97ba957ec4250be3f413fa5a
billblanks60-sudo <billblanks60@gmail.com>
2026-02-20 09:04:23 +0300

    first commit

--- bootstrapdemo_app/urls.py
+++ bootstrapdemo_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-     path('',views.home,name='index.html'),
+     path('',views.home,name='index'),
+     path('about/',views.about,name='about'),
      
 ]
